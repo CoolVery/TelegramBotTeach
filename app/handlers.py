f00c6f6487b5bb50030086db12c42e39e71ce980
CoolVery <georgekuklevwork@gmail.com>
2024-10-21 19:30:08 +0300

    added state groups

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,18 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
 #Подключили кнопки
 import app.keyboards as kb
 
 #Мы создали роутер - он делает роль диспетчера
 router = Router()
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 #Хендлер - обработчик входящих сообщений
 #Декоратор для диспетчера
@@ -50,3 +57,21 @@ async def cool(callback: CallbackQuery):
     await callback.message.edit_text('Дароу', reply_markup=await kb.inline_cars())
     #так бот ответит следующим сообщением
     #await callback.message.answer('Дароу', reply_markup=await kb.inline_cars())
+    
+@router.message(Command('reg'))
+async def reg_one(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer('Введите имя')
+    
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer('Введите имя телефона')
+    
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(f'Все круть {data["name"]}   {data["number"]}')
+    await state.clear()
